3498d2582fbd85a28c8e8edd02acf09493d68886
colbygatte <colbygatte@gmail.com>
2016-11-07 22:24:09 -0600

    ProductView2.xib is working

---
+++ AmaClone/ProductView2.swift
@@ -0,0 +1,21 @@
+//
+//  ProductView2.swift
+//  AmaClone
+//
+//  Created by Colby Gatte on 11/7/16.
+//  Copyright © 2016 colbyg. All rights reserved.
+//
+
+import UIKit
+
+class ProductView2: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
